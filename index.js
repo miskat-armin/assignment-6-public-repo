c7155b77eca7f44b1e6c618bba2a4705200ff8cb
miskat-armin <miskatarmin123@gmail.com>
2023-09-01 12:09:43 +0600

    finished the selected tab style

--- index.js
+++ index.js
@@ -8,15 +8,20 @@ const handleCategory = async () => {
     data.data.forEach((category) => {
         const div = document.createElement('div');
         div.innerHTML = `
-            <a onclick="handleLoadCategory('${category.category_id}')" class="tab bg-[#25252526] rounded-sm px-4 font-medium text-base">${category.category}</a>
+            <a onclick="handleLoadCategory('${category.category_id}', this)" id="${category.category}" class="tab rounded-sm px-4 font-medium text-base">${category.category}</a>
         `
 
         tabContainer.appendChild(div);
     })
 
+    const defaultTab = document.getElementById('All');
+
+    defaultTab.classList.add('selected');
+
 }
 
-const handleLoadCategory = async (categoryID) => {
+const handleLoadCategory = async (categoryID, selectedTab) => {
+
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryID}`);
     const data = await response.json();
 
@@ -41,8 +46,8 @@ const handleLoadCategory = async (categoryID) => {
                                         <p>${cardItem?.authors[0]?.profile_name}</p>
                                         <img class="w-[20px] h-[20px]" src="./blue-tick.jpg"
                                     </div>` :
-                                    `${cardItem?.authors[0]?.profile_name}`
-                                    }
+                `${cardItem?.authors[0]?.profile_name}`
+            }
                                 </p>
                             </div>
                             <p class="font-normal text-sm text-[#171717B3]">${cardItem.others?.views} views</p>
@@ -57,6 +62,16 @@ const handleLoadCategory = async (categoryID) => {
 
     // console.log(data.data);
     // console.log(categoryID);
+    const tabContainer = document.getElementById('tab-container');
+
+    if (selectedTab !== null) {
+        const tabs = tabContainer.querySelectorAll('.tab');
+        tabs.forEach(tab => tab.classList.remove('selected'));
+
+        selectedTab.classList.add('selected');
+    }
 }
 
 handleCategory();
+handleLoadCategory('1000', null);
+
